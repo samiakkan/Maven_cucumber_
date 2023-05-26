c98b675ccccc805e68ffa2f2d9d765e5e27e7083
Sam <akkansami9@gmail.com>
2023-05-25 23:38:53 -0400

    jenkins update 2

--- src/test/java/Utilities/DriverClass.java
+++ src/test/java/Utilities/DriverClass.java
@@ -32,7 +32,7 @@ public class DriverClass {
                 default:
                     ChromeOptions options = new ChromeOptions();
                     options.addArguments("--remote-allow-origins=*"); // To solve the error with Chrome v111
-                    options.addArguments("--headless", "--no-sandbox", "--disable-dev-shm-usage", "--disable-gpu", "--window-size=1400,2400");
+                    options.addArguments("--headless", "--no-sandbox", "--disable-dev-shm-usage", "--disable-gpu", "--window-size=1400,2400"); // to condition dimension for bigger size
                     threadDriver.set(new ChromeDriver(options));
             }
         }
